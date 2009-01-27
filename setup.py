82b27b6f57f560197e7ed4eebee009c5669452d6
Dan Korostelev <nadako@gmail.com>
2009-01-27 13:04:14 +0000

    Tag 3.4.6

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.file',
-      version = '3.5.0dev',
+      version = '3.4.6',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='File and Image -- Zope 3 Content Components',
       long_description=(
           read('README.txt')
@@ -49,7 +49,7 @@ setup(name='zope.app.file',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.file',
+      url='http://pypi.python.org/pypi/zope.app.file',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
